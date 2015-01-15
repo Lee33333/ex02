31a88d9ee373a816a39aca082d67349c4e578c83
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 16:31:09 -0800

    First working version

---
+++ arithmetic.py
@@ -0,0 +1,32 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def square(num1):
+    return num1 ** 2
+
+def cube(num1):
+    return num1 ** 3
+
+def power(num1, num2):
+    return num1 ** num2
+
+def mod(num1, num2):
+    return num1 % num2
+
+print add(1 , 2)
+print subtract(1 , 2)
+print multiply(1 , 2)
+print divide(1.0 , 2.0)
+print square(3)
+print cube(2)
+print power(2 , 2)
+print mod (10 , 3)
